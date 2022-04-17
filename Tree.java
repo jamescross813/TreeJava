3c26ee1e4361298e3ed9061ed161ad9ad83d3592
jamescross813 <crossjames813@gmail.com>
2022-04-17 18:00:34 -0500

    done

---
+++ Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree{
+  public static void main(String[] args){
+// Will introduce self and plant a tree
+    System.out.println("Hey there, I'm James and I'm learnign to code Java!");
+    System.out.println("I'm going to plant a tree today'");
+    System.out.println("  ^  ");
+    System.out.println(" / \\ ");
+    System.out.println("/   \\");
+    System.out.println("  |  ");
+    System.out.println("  |  ");  
+  }
+}
